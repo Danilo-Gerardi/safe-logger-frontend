a5b3771d77b0fc03a806bd8b0c6f2dd13cb5d615
Danilo Faria <dfaria@ciandt.com>
2021-11-12 00:58:54 -0300

    Colocando no github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+import Logs from './pages/Logs';
+import { AuthProvider } from './providers/auth';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Login} />
+            <Route path="/signin" component={Login} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/home" component={Home} />
+            <Route path="/logs" component={Logs} />
+          </Switch>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
