85bbe12f8f7ee553f122ca11ddf5a5bd4affedb3
Danilo Faria <dfaria@ciandt.com>
2021-11-15 02:46:48 -0300

    cenarios de admitir colab funcionando

--- src/pages/HomeBusiness.jsx
+++ src/pages/HomeBusiness.jsx
@@ -4,14 +4,17 @@ import moment from 'moment';
 
 import 'moment/locale/pt-br'
 import '../styles/session.scss';
+import '../styles/home.scss';
 import { useAuth } from "../providers/auth";
 import admitCollaborator from "../service/admitCollaborator";
+import { cpf } from 'cpf-cnpj-validator';
 
 
 const HomeBusiness = props => {
 
+    const [classe, setClasse] = useState('')
     const { organization } = useAuth();
-    const [cpf, setCpf] = useState('');
+    const [cpfNum, setCpfNum] = useState('');
     const history = useHistory();
 
     function toUpperCaseInTheFirstLetter(str) {
@@ -22,21 +25,38 @@ const HomeBusiness = props => {
 
     function admitColaborator(e) {
         e.preventDefault();
-        admitCollaborator({
-            userDocument: cpf,
-            organizationDocument:
-                JSON.parse(localStorage.getItem('organization')).document
-        }, () => turnMessageGreen(), () => hideMessage())
+        admitCollaborator(
+            { userDocument: cpfNum },
+            onSuccessMessage,
+            onErrorMessage)
     }
 
-    function turnMessageGreen() {
+    function onSuccessMessage() {
         let el = document.getElementById("cpfInput");
         el.style.color = "green";
     }
 
-    function hideMessage() {
+    function onErrorMessage(errorMessage) {
         let el = document.getElementById("cpfInput");
-        el.style.color = "";
+        el.style.color = "black";
+        if (errorMessage === "No message available") {
+            el.innerText = "";
+        } else if (errorMessage === "Collaborator already admitted") {
+            el.innerText = "Este usuário já é um colaborador.";
+        } else if (errorMessage === "User does not exist") {
+            el.innerText = "Colaborador não encontrado.";
+        }
+    }
+
+    function validateCpf(cpfNumber) {
+        setCpfNum(cpfNumber)
+        setClasse("red-border")
+
+        if (cpf.isValid(cpfNumber)) {
+            setClasse("green-border")
+        } else {
+            setClasse("red-border")
+        }
     }
 
     const dayOfTheWeek = toUpperCaseInTheFirstLetter(moment().format('dddd'));
@@ -81,9 +101,11 @@ const HomeBusiness = props => {
                     <div class="container-form">
                         <div class="times">
                             <input
+                                className={classe}
                                 type="text"
-                                onChange={e => setCpf(e.target.value)}
-                                value={cpf}
+                                onChange={e => validateCpf(e.target.value)}
+                                value={cpfNum}
+                                onClick={e => e.target.value = ""}
                             />
                         </div>
                         <div id="cpfInput" className="msg-colaborator-admitted">Coloborador admitido!</div>
